ff60829770280189584835192ca6d58b73d949ca
Malaj Alexandre <alexandre.malaj@gmail.com>
2018-11-27 17:05:27 +0100

    feat: check all params type

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Require Third-Party Dependencies
+const is = require("@slimio/is");
+
 /**
- * @type {WeakMap<QueueMap, Map<String|Number, any[]>>}
+ * @type {WeakMap<QueueMap, Map<String|Number|Symbol, any[]>>}
  */
 const Queues = new WeakMap();
 
@@ -15,12 +18,22 @@ class QueueMap {
     }
 
     /**
+     * @memberof #QueueMap
      * @method enqueue
      * @param {!String|Number|Symbol} id key identifier
-     * @param {*} value value to enqueue
+     * @param {!*} value value to enqueue
+     *
+     * @throws {TypeError}
      * @returns {void}
      */
     enqueue(id, value) {
+        if (!is.nullOrUndefined(id) && !is.string(id) && !is.number(id) && !is.symbol(id)) {
+            throw new TypeError("id param must be typeof <string|number|Symbol>");
+        }
+        if (is.nullOrUndefined(value)) {
+            throw new TypeError("value param must be define");
+        }
+
         const q_ = Queues.get(this);
         if (q_.has(id)) {
             q_.get(id).push(value);
@@ -31,11 +44,18 @@ class QueueMap {
     }
 
     /**
+     * @memberof #QueueMap
      * @method dequeue
      * @param {!String|Number|Symbol} id key identifier
+     *
+     * @throws {TypeError}
      * @returns {any}
      */
     dequeue(id) {
+        if (!is.nullOrUndefined(id) && !is.string(id) && !is.number(id) && !is.symbol(id)) {
+            throw new TypeError("id param must be typeof <string|number|Symbol>");
+        }
+
         const q_ = Queues.get(this);
         if (!q_.has(id)) {
             return null;
@@ -50,11 +70,17 @@ class QueueMap {
     }
 
     /**
+     * @memberof #QueueMap
      * @method dequeueAll
      * @param {!String|Number|Symbol} id key identifier
+     *
+     * @throws {TypeError}
      * @returns {Iterator<any>}
      */
     * dequeueAll(id) {
+        if (!is.nullOrUndefined(id) && !is.string(id) && !is.number(id) && !is.symbol(id)) {
+            throw new TypeError("id param must be typeof <string|number|Symbol>");
+        }
         const q_ = Queues.get(this);
         if (!q_.has(id)) {
             throw new Error(`Unknow queue id ${id}`);
@@ -68,6 +94,7 @@ class QueueMap {
     }
 
     /**
+     * @memberof #QueueMap
      * @method ids
      * @returns {String[]}
      */
@@ -76,11 +103,18 @@ class QueueMap {
     }
 
     /**
+     * @memberof #QueueMap
      * @method idLength
-     * @param {any} id id
+     * @param {!String|Number|Symbol} id id
+     *
+     * @throws {TypeError}
      * @returns {Number}
      */
     idLength(id) {
+        if (!is.nullOrUndefined(id) && !is.string(id) && !is.number(id) && !is.symbol(id)) {
+            throw new TypeError("id param must be typeof <string|number|Symbol>");
+        }
+
         return Queues.get(this).get(id).length;
     }
 }
